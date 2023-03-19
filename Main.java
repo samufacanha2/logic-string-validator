611899b42d02a00430ae1816f0a0b54aa6a4c2b3
Samuel Façanha <samufacanha@gmail.com>
2023-03-19 13:50:12 -0300

    added fbf validation

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         String formula = "(p→q";
         Validacao v = new Validacao(formula);
-        v.mostraCaracteres();
+        System.out.println(v.isFbf());
         System.out.println(Tableaux.checkFormula(formula));
     }
 }
