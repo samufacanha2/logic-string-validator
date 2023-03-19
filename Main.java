a61ccfe89193ac9cb2a195012f7fdf7c4944d7f2
Samuel Façanha <samufacanha@gmail.com>
2023-03-19 18:45:46 -0300

    linear validation working

--- Main.java
+++ Main.java
@@ -1,18 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        // String formula = "(p→q";
-        // Validacao v = new Validacao(formula);
-        // System.out.println(v.isFbf());
-        // System.out.println(Tableaux.checkFormula(formula));
-        String formula = "((p^q)^~p)";
+        Scanner input = new Scanner(System.in);
 
-        System.out.println("formula: " + formula);
+        System.out.print("Enter a logical expression: ");
+        String expression = input.nextLine();
 
-        System.out.println(Tableaux.checkFormula(formula));
+        Validacao v = new Validacao(expression);
 
-        // Node node = new Node(formula);
-        // node.print();
+        do {
+            System.out.println("Invalid expression, please enter a valid one.");
+            expression = input.nextLine();
+            v = new Validacao(expression);
+        } while (!v.isFbf());
 
+        // Prompt the user to enter values for the variables
+        for (char variable : Evaluator.getVariables(expression)) {
+            System.out.print("Enter a value for " + variable + " (true or false): ");
+            boolean value = input.nextBoolean();
+            expression = expression.replace(variable, value ? 'T' : 'F');
+        }
+
+        while (expression.contains("~T") || expression.contains("~F")) {
+            expression = expression.replace("~T", "F");
+            expression = expression.replace("~F", "T");
+        }
+
+        // Evaluate the logical expression
+        boolean result = Evaluator.evaluateExpression(expression);
+
+        // Print the result
+        System.out.println("The result of the expression " + expression + " is: " + result);
+
+        input.close();
     }
+
 }
